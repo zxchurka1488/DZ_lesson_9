fcd3fec384f90d5fb1b79fb5f68a9d24c0e6165f
zxchurka1488 <139868359+zxchurka1488@users.noreply.github.com>
2023-08-19 14:09:48 +0300

    Дз урок 9 модуль 4

---
+++ advertisements/app_advertisements/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+class Advertisement(models.Model):
+    class Meta:
+        db_table = "advertisements"
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+
+
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text = "Уместен торг или нет")
+    created_at = models.DateField("Опубликовано", auto_now_add=True)
+    updated_at = models.DateField("Обновлено", auto_now=True)
+    user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
+    image = models.ImageField("Изображение", upload_to="advertisements/")
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description="Изображение")
+    def img_show(self):
+        from django.utils.html import format_html
+        if self.image != "":
+
+            return format_html('<img src="/media/%s" width="50" height="50" />' % (self.image))
+        self.image = 'advertisements/adv1.png'
+        return format_html('<img src="/media/%s" width="50" height="50" />' % (self.image))
+
+'''''
+@admin.display(description="Дата создания")
+def created_date(self):
+    from django.utils import timezone
+    from django.utils.html import format_html
+    if self.created_at.date() == timezone.now().date():
+        created_time = self.created_at.time().strftime("%H:%M:%S")
+        return format_html('<Span style="color: green; font-weight: bold:">Сегодня в {}</span>', created_time)
+    return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+'''''
---
+++ advertisements/app_advertisements/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import index, top_sellers
+
+urlpatterns = [
+    path('', index),
+    path('top-sellers/', top_sellers)
+]
